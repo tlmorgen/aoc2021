8647f9837cac55701193919afed6de82fd10ee50
tlmorgen <tlmorgen@gmail.com>
2021-12-02 02:50:44 -0500

    part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,6 +1,8 @@
 use std::fs;
 use clap::{App, Arg};
 
+const WIN_SZ: usize = 3;
+
 fn main() {
     let args = App::new("AoC-2021-01")
         .version("0.0.1")
@@ -11,16 +13,22 @@ fn main() {
             .index(1))
         .get_matches();
 
-    let lines = fs::read_to_string(args.value_of("FILE").unwrap()).expect("unable to read file");
     let mut incrs = 0;
-    let mut last_depth = -1;
-    let depths: Vec<&str> = lines.split("\n").collect();
-    for (i, depth) in depths.iter().enumerate() {
-        let curr_depth = depth.parse::<isize>().expect("not a num");
-        if i > 0 && curr_depth > last_depth {
+    let mut last_window: isize = -1;
+    let depths: Vec<isize> = fs::read_to_string(args.value_of("FILE").unwrap())
+        .expect("unable to read file")
+        .split("\n")
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|s| s.parse::<isize>().expect("not a num"))
+        .collect();
+
+    for i in 0..(depths.len() - WIN_SZ + 1) {
+        let curr_window = depths[i..i+WIN_SZ].iter().sum();
+        if i > 0 && last_window < curr_window {
             incrs += 1;
         }
-        last_depth = curr_depth;
+        last_window = curr_window;
     }
 
     println!("{}", incrs)
