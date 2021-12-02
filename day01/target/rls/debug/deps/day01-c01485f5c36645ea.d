1b203220bde4eb0b1b9aca20d9c5b4cf00c13fd1
tlmorgen <tlmorgen@gmail.com>
2021-12-01 19:22:32 -0500

    init

---
+++ day01/target/rls/debug/deps/day01-c01485f5c36645ea.d
@@ -0,0 +1,5 @@
+c:\Users\Taylor\projects\aoc2021\day01\target\rls\debug\deps\day01-c01485f5c36645ea.rmeta: src\main.rs
+
+c:\Users\Taylor\projects\aoc2021\day01\target\rls\debug\deps\day01-c01485f5c36645ea.d: src\main.rs
+
+src\main.rs:
