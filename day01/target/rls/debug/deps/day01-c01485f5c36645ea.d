bfdde87f90911bc8eca9ad36474b00888ea881a6
tlmorgen <tlmorgen@gmail.com>
2021-12-01 19:25:45 -0500

    remove target

--- day01/target/rls/debug/deps/day01-c01485f5c36645ea.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Taylor\projects\aoc2021\day01\target\rls\debug\deps\day01-c01485f5c36645ea.rmeta: src\main.rs
-
-c:\Users\Taylor\projects\aoc2021\day01\target\rls\debug\deps\day01-c01485f5c36645ea.d: src\main.rs
-
-src\main.rs:
--- day01/target/rls/debug/deps/day01-f63032d1f78a06fa.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Taylor\projects\aoc2021\day01\target\rls\debug\deps\day01-f63032d1f78a06fa.rmeta: src\main.rs
-
-c:\Users\Taylor\projects\aoc2021\day01\target\rls\debug\deps\day01-f63032d1f78a06fa.d: src\main.rs
-
-src\main.rs:
