bab57175993ad7d14e07c7c60a2159dc1a6a0d84
tlmorgen <tlmorgen@gmail.com>
2021-12-02 19:57:01 -0500

    part 1

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,5 +1,7 @@
 use std::fs;
 use clap::{App, Arg};
+use std::panic;
+use itertools::Itertools;
 
 fn main() {
     let args = App::new("AoC-2021-01")
@@ -10,4 +12,20 @@ fn main() {
             .required(true)
             .index(1))
         .get_matches();
+
+    let (x, y): (isize, isize) = fs::read_to_string(args.value_of("FILE").unwrap())
+        .expect("unable to read file")
+        .split_whitespace()
+        .tuples()
+        .fold((0, 0), |(x, y), (dir, sqty)| {
+            let qty = sqty.parse::<isize>().unwrap_or_else(|_| panic!("not a num: {}", sqty));
+            match dir {
+                "forward" => (x + qty, y),
+                "up" => (x, y + qty),
+                "down" => (x, y - qty),
+                _ => panic!("unsupported direction: {}", dir)
+            }
+        });
+        
+    println!("{}", x * -y);
 }
