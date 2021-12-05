8b761bcfc9039a922f0d60c7a113a4e3bfbd3dd1
tlmorgen <tlmorgen@gmail.com>
2021-12-04 23:41:25 -0500

    consts

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use day::Day;
 use chrono::{Utc, TimeZone, Datelike};
 use chrono_tz::US::Eastern;
 
+const ARG_DAY: &'static str = "day";
+const ARG_TEST: &'static str = "test";
 const DAY_MAKERS: &'static [fn(&str) -> Box<dyn  Day>] = &[
     days::day1::Day1::from_content,
     days::day2::Day2::from_content,
@@ -21,21 +23,21 @@ fn main() {
     let app = App::new("AoC-2021-01")
         .version("0.0.1")
         .about("Advent of Code 2021")
-        .arg(Arg::with_name("day")
+        .arg(Arg::with_name(ARG_DAY)
             .help("which day to run (default: the current day of December ET)")
             .index(1))
-        .arg(Arg::with_name("test")
+        .arg(Arg::with_name(ARG_TEST)
             .help("test mode (the sample data)")
             .short("t"))
         .get_matches();
 
-    let day_num: usize = match app.value_of("day") {
+    let day_num: usize = match app.value_of(ARG_DAY) {
         Some(day) => day.parse().expect("day is not a num"),
         None => Eastern.from_utc_datetime(&Utc::now().naive_utc())
             .day() as usize
     };
     let day_idx = day_num - 1;
-    let test: bool = app.occurrences_of("test") > 0;
+    let test: bool = app.occurrences_of(ARG_TEST) > 0;
     let content_path = format!("./day{:02}/{}.txt", day_num, if test {"test"} else {"input"});
     let content = fs::read_to_string(&content_path).expect("io");
 
