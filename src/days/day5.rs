1ff2d55e525d3e717f49ece025d97f264e5ed7fc
tlmorgen <tlmorgen@gmail.com>
2021-12-05 03:33:57 -0500

    part 2

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -3,7 +3,7 @@ use std::cmp;
 use itertools::Itertools;
 
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Point {
     x: usize,
     y: usize,
@@ -17,43 +17,40 @@ struct Line {
 
 pub struct Day5 {
     lines: Vec<Line>,
+    max_x: usize,
+    max_y: usize,
 }
 
 impl Day5 {
     pub fn from_content(content: &str) -> Box<dyn Day> {
-        Box::new(Day5 {
-            lines: content.split_whitespace()
-                .tuples()
-                .map(|(a, _, b)| {
-                    Line {
-                        a: Point::from_comma_string(a),
-                        b: Point::from_comma_string(b),
-                    }
+        let lines: Vec<Line> = content.split_whitespace()
+            .tuples()
+            .map(|(a, _, b)| Line {
+                    a: Point::from_comma_string(a),
+                    b: Point::from_comma_string(b),
                 })
-                .collect()
+            .collect();
+        let (max_x, max_y) = lines.iter()
+            .fold((0, 0), |(max_x, max_y), line| {
+                (cmp::max(max_x, cmp::max(line.a.x, line.b.x)),
+                 cmp::max(max_y, cmp::max(line.a.y, line.b.y)))
+            });
+
+        Box::new(Day5 {
+            lines,
+            max_x,
+            max_y,
         })
     }
 }
 
 impl Day for Day5 {
     fn part1(&mut self) -> isize {
-        let (max_x, max_y) = self.lines.iter()
-            .fold((0, 0), |(max_x, max_y), line| {
-                (cmp::max(max_x, cmp::max(line.a.x, line.b.x)),
-                 cmp::max(max_y, cmp::max(line.a.y, line.b.y)))
-            });
-
-        let mut grid = vec![vec![0 as usize; (max_y + 1)]; (max_x + 1)];
+        let mut grid = vec![vec![0; self.max_y + 1]; self.max_x + 1];
         for line in self.lines.iter() {
             if line.a.x == line.b.x || line.a.y == line.b.y {
-                let lx = cmp::min(line.a.x, line.b.x);
-                let hx = cmp::max(line.a.x, line.b.x);
-                for x in lx..=hx {
-                    let ly = cmp::min(line.a.y, line.b.y);
-                    let hy = cmp::max(line.a.y, line.b.y);
-                    for y in ly..=hy {
-                        grid[x][y] += 1;
-                    }
+                for point in line {
+                    grid[point.x][point.y] += 1;
                 }
             }
         }
@@ -65,7 +62,17 @@ impl Day for Day5 {
     }
 
     fn part2(&mut self) -> isize {
-        0
+        let mut grid = vec![vec![0; self.max_y + 1]; self.max_x + 1];
+        for line in self.lines.iter() {
+            for point in line {
+                grid[point.x][point.y] += 1;
+            }
+        }
+
+        grid.iter()
+            .flatten()
+            .filter(|&&count| count > 1)
+            .count() as isize
     }
 }
 
@@ -73,18 +80,53 @@ impl Point {
     fn from_comma_string(s: &str) -> Point {
         s.splitn(2, ",")
             .tuples()
-            .map(|(x, y)| {
-                Point {
+            .map(|(x, y)| Point {
                     x: x.parse().expect("not a num"),
                     y: y.parse().expect("not a num"),
-                }
-            })
+                })
             .next()
             .expect("no points")
     }
 }
 
-fn print(grid: &Vec<Vec<usize>>) {
-    grid.iter().for_each(|row| eprintln!("{:?}", row));
-    eprintln!();
+struct LineIntoIterator<'a> {
+    line: &'a Line,
+    next_point: Point,
+    x_incr: isize,
+    y_incr: isize,
+    done: bool,
+}
+
+impl<'a> IntoIterator for &'a Line {
+    type Item = Point;
+    type IntoIter = LineIntoIterator<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        LineIntoIterator {
+            line: self,
+            next_point: self.a,
+            x_incr: if self.a.x == self.b.x { 0 } else if self.a.x < self.b.x { 1 } else { -1 },
+            y_incr: if self.a.y == self.b.y { 0 } else if self.a.y < self.b.y { 1 } else { -1 },
+            done: false,
+        }
+    }
+}
+
+impl<'a> Iterator for LineIntoIterator<'a> {
+    type Item = Point;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.done {
+            None
+        } else if self.next_point.x == self.line.b.x && self.next_point.y == self.line.b.y {
+            self.done = true;
+            Some(self.next_point)
+        } else {
+            let this_point = self.next_point;
+            self.next_point = Point {
+                x: (self.next_point.x as isize + self.x_incr) as usize,
+                y: (self.next_point.y as isize + self.y_incr) as usize,
+            };
+            Some(this_point)
+        }
+    }
 }
