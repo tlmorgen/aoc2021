1ef93cccf306d5a994bf4fee3d0e80a1736b02aa
tlmorgen <tlmorgen@gmail.com>
2021-12-07 00:50:48 -0500

    part 2; little bit of memoization

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -1,7 +1,11 @@
 use super::super::day::Day;
+use std::cmp;
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 
 pub struct Day7 {
-    crab_hpos: Vec<usize>
+    crab_hpos: Vec<usize>,
+    linear_costs: HashMap<usize, usize>
 }
 
 impl Day7 {
@@ -11,12 +15,14 @@ impl Day7 {
                 .trim()
                 .split(",")
                 .filter_map(|n| n.parse().ok())
-                .collect()
+                .collect(),
+            linear_costs: HashMap::new()
         }))
     }
 }
 
 impl Day for Day7 {
+
     fn part1(&mut self) -> isize {
         let min = self.crab_hpos.iter().min().unwrap();
         let max = self.crab_hpos.iter().max().unwrap();
@@ -26,12 +32,42 @@ impl Day for Day7 {
             for crab in &self.crab_hpos {
                 cost += (p as isize - *crab as isize).abs();
             }
-            min_cost = std::cmp::min(min_cost, cost as usize);
+            min_cost = cmp::min(min_cost, cost as usize);
         }
         min_cost as isize
     }
 
     fn part2(&mut self) -> isize {
-        0
+        let crabs = self.crab_hpos.clone();
+        let min = self.crab_hpos.iter().min().unwrap();
+        let max = self.crab_hpos.iter().max().unwrap();
+        let mut min_cost = usize::MAX;
+        for p in *min..*max {
+            let mut cost: usize = 0;
+            for crab in crabs.iter() {
+                cost += self.mv_cost((p as isize - *crab as isize).abs() as usize);
+            }
+            min_cost = cmp::min(min_cost, cost);
+        }
+        min_cost as isize
+    }
+}
+
+impl Day7 {
+    fn mv_cost(&mut self, mut dist: usize) -> usize {
+        match self.linear_costs.entry(dist) {
+            Entry::Occupied(entry) => {
+                *entry.get()
+            }
+            Entry::Vacant(slot) => {
+                let mut cost = 0;
+                while dist > 0 {
+                    cost += dist;
+                    dist -= 1;
+                }
+                slot.insert(cost);
+                cost
+            }
+        }
     }
 }
