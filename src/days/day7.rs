a5cdba7afb96fe07023b4a7240777bdca768dbd0
tlmorgen <tlmorgen@gmail.com>
2021-12-07 00:13:42 -0500

    init day 7

---
+++ src/days/day7.rs
@@ -0,0 +1,19 @@
+use super::super::day::Day;
+
+pub struct Day7 {}
+
+impl Day7 {
+    pub fn from_content(content: &str) -> Result<Box<dyn Day>, &'static str> {
+        Err("not implemented")
+    }
+}
+
+impl Day for Day7 {
+    fn part1(&mut self) -> isize {
+        0
+    }
+
+    fn part2(&mut self) -> isize {
+        0
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,8 @@ const DAY_MAKERS: &'static [DayMaker] = &[
     days::day3::Day3::from_content,
     days::day4::Day4::from_content,
     days::day5::Day5::from_content,
-    days::day6::Day6::from_content
+    days::day6::Day6::from_content,
+    days::day7::Day7::from_content
 ];
 
 fn main() -> ExitCode {
