13d8f925f74ea2a4d647dcbb1a77ebfe0fbf4f9a
tlmorgen <tlmorgen@gmail.com>
2021-12-10 19:48:28 -0500

    part 2

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,5 +1,7 @@
 use super::super::day::Day;
 use std::collections::{HashSet, HashMap};
+use bimap::BiHashMap;
+use itertools::Itertools;
 
 pub struct Day10 {
     braces: Vec<Vec<char>>
@@ -41,6 +43,48 @@ impl Day for Day10 {
     }
 
     fn part2(&mut self) -> isize {
-        0
+        let peers: BiHashMap<char, char> = BiHashMap::from_iter(vec![(')', '('), (']', '['), ('}', '{'), ('>', '<')]);
+        let point_vals: HashMap<char, isize> = HashMap::from([(')', 1), (']', 2), ('}', 3), ('>', 4)]);
+
+        let mut entry_points: Vec<isize> = Vec::new();
+        for entry in &self.braces {
+            let mut incomplete = true;
+            let mut stack: Vec<char> = Vec::new();
+            for c in entry {
+                match peers.get_by_left(&c) {
+                    None => {
+                        // must be an open
+                        stack.push(*c);
+                    }
+                    Some(proper_peer) => {
+                        // must be a close
+                        match stack.pop() {
+                            None => {
+                                // too many closes
+                                incomplete = false;
+                                break;
+                            },
+                            Some(found_peer) => {
+                                if found_peer != *proper_peer { // wrong close
+                                    incomplete = false;
+                                    break;
+                                } // else normal close
+                            }
+                        }
+                    }
+                }
+            }
+
+            let mut sum = 0isize;
+            if incomplete {
+                let points = stack.iter().rev()
+                    .fold(0isize, |points, open_brace| {
+                        (points * 5) + point_vals.get(peers.get_by_right(open_brace).unwrap()).unwrap()
+                    });
+                entry_points.push(points);
+            }
+        }
+        let entry_points: Vec<isize> = entry_points.into_iter().sorted().collect();
+        entry_points[entry_points.len() / 2]
     }
 }
