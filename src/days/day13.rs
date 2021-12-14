20277b2868cea76fc8dc2a7c0742dc217fbb0c64
tlmorgen <tlmorgen@gmail.com>
2021-12-13 20:59:17 -0500

    playing around cleanup

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -4,25 +4,23 @@ use array2d::Array2D;
 
 pub struct Day13 {
     points: Vec<Pos>,
-    folds: Vec<Fold>
+    folds: Vec<Fold>,
 }
 
 impl Day13 {
     pub fn from_content(content: &str) -> Result<Box<dyn Day>, &'static str> {
-        let (points, folds) = content.lines()
-            .fold((Vec::new(), Vec::new()), |(mut points, mut folds), line| {
-                if line.contains(',') {
-                    points.push(Pos::from_str(line))
-                } else if line.contains('=') {
-                    line.split_whitespace().filter(|word| word.contains('='))
-                        .for_each(|word| folds.push(Fold::from_str(word)))
-                };
-                (points, folds)
-            });
-        Ok(Box::new(Day13 {
-            points,
-            folds
-        }))
+        let mut today = Day13 {
+            points: Vec::new(),
+            folds: Vec::new()
+        };
+        for word in content.split_whitespace() {
+            if word.contains(',') {
+                today.points.push(Pos::from_str(word).unwrap());
+            } else if word.contains('=') {
+                today.folds.push(Fold::from_str(word).unwrap())
+            }
+        }
+        Ok(Box::new(today))
     }
 }
 
@@ -33,88 +31,82 @@ impl Day for Day13 {
     }
 
     fn part2(&mut self) -> isize {
-        let folded: Vec<Pos> = self.folds.iter().fold(self.points.clone(), |points, fold_instr| {
-            fold_instr.fold_points(points)
-        });
+        let folded: Vec<Pos> = self.folds.iter()
+            .fold(self.points.clone(),
+                  |points, fold_instr| fold_instr.fold_points(points));
         let max_x = folded.iter().map(|pos| pos.x).max().unwrap();
         let max_y = folded.iter().map(|pos| pos.y).max().unwrap();
-        let mut grid: Array2D<char> = Array2D::filled_with(' ', (max_y + 1) as usize, (max_x + 1) as usize);
+        let mut grid: Array2D<char> = Array2D::filled_with(' ', max_y + 1, max_x + 1);
         for pos in folded {
-            grid.set(pos.y as usize, pos.x as usize, '#');
+            grid.set(pos.y, pos.x, '#').unwrap();
         }
-        grid.rows_iter().for_each(|mut col_iter| {
-            eprintln!("{}", col_iter.join(""));
+        grid.rows_iter().for_each(|mut cell_iter| {
+            eprintln!("{}", cell_iter.join(""));
         });
         0
     }
 }
 
-#[derive(Ord, PartialOrd, Eq, PartialEq, Clone, Hash, Debug)]
-struct Pos {
-    x: isize,
-    y: isize
-}
-
-#[derive(Eq, PartialEq)]
+#[derive(PartialEq)]
 enum FoldDir {
     Row,
-    Col
+    Col,
 }
 
 struct Fold {
-    location: isize,
-    fold: FoldDir
+    location: usize,
+    dir: FoldDir,
 }
 
 impl Fold {
-    fn from_str(word: &str) -> Fold {
-        let mut iter = word.splitn(2, '=');
-        let fold = match iter.next().unwrap() {
-            "x" => FoldDir::Col,
-            "y" => FoldDir::Row,
-            _ => panic!("unsupported fold direction")
-        };
-        let location = iter.next().unwrap().parse::<isize>().unwrap();
-        Fold {
-            location,
-            fold
+    fn from_str(word: &str) -> Option<Fold> {
+        word.split('=')
+            .tuples()
+            .map(|(s_dir, s_loc)| Fold {
+                dir: match s_dir {
+                    "x" => FoldDir::Col,
+                    "y" => FoldDir::Row,
+                    _ => panic!("unsupported fold direction")
+                },
+                location: s_loc.parse().unwrap()
+            })
+            .next()
+    }
+
+    fn fold_point(&self, pos: Pos) -> Pos {
+        let mut pos: Pos = pos.clone();
+        if self.dir == FoldDir::Row {
+            if pos.y > self.location {
+                pos.y = (self.location * 2) - pos.y;
+            }
+        } else {
+            if pos.x > self.location {
+                pos.x = (self.location * 2) - pos.x;
+            }
         }
+        pos
     }
 
     fn fold_points(&self, points: Vec<Pos>) -> Vec<Pos> {
-        if self.fold == FoldDir::Row {
-            points.into_iter().map(|pos| {
-                if pos.y > self.location {
-                    Pos {
-                        x: pos.x,
-                        y: self.location - (pos.y - self.location),
-                    }
-                } else {
-                    pos
-                }
-            }).unique().collect()
-        } else {
-            points.into_iter().map(|pos| {
-                if pos.x > self.location {
-                    Pos {
-                        x: self.location - (pos.x - self.location),
-                        y: pos.y,
-                    }
-                } else {
-                    pos
-                }
-            }).unique().collect()
-        }
+        points.into_iter()
+            .map(|pos| self.fold_point(pos))
+            .unique()
+            .collect()
     }
 }
 
+#[derive(Eq, PartialEq, Clone, Debug, Hash)]
+struct Pos {
+    x: usize,
+    y: usize,
+}
+
 impl Pos {
-    fn from_str(word: &str) -> Pos {
+    fn from_str(word: &str) -> Option<Pos> {
         word.split(',')
             .flat_map(|n| n.parse().ok())
             .tuples()
-            .map(|(row,  col)| Pos { x: row, y: col })
+            .map(|(x, y)| Pos { x, y })
             .next()
-            .expect("no pos on this line")
     }
 }
