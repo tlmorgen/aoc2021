18bbd2f02f268bb2050bc2c6e8dda12aeb9eaccc
tlmorgen <tlmorgen@gmail.com>
2021-12-15 09:38:44 -0500

    init day 15

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,8 @@ const DAY_MAKERS: &'static [DayMaker] = &[
     days::day11::Day11::from_content,
     days::day12::Day12::from_content,
     days::day13::Day13::from_content,
-    days::day14::Day14::from_content
+    days::day14::Day14::from_content,
+    days::day15::Day15::from_content
 ];
 
 fn main() -> ExitCode {
