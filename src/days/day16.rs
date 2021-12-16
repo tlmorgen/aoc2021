baabd5541d55a6ddc7a6c52275cbac9b2ad1d134
tlmorgen <tlmorgen@gmail.com>
2021-12-16 16:12:48 -0500

    part 2

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -28,29 +28,43 @@ impl Day for Day16 {
     }
 
     fn part2(&mut self) -> isize {
-        0
+        let mut iter = self.bits.iter();
+        let packet = Packet::from_iter(&mut iter);
+        packet.execute()
     }
 }
 
 #[derive(Debug, Eq, PartialEq)]
 enum PacketType {
     Literal,
-    Operation
+    Sum,
+    Product,
+    Min,
+    Max,
+    Gt,
+    Lt,
+    Eq
 }
 
 impl PacketType {
-    fn from_num(num: usize) -> PacketType {
-        if num == 4 {
-            PacketType::Literal
-        } else {
-            PacketType::Operation
+    fn from_num(num: isize) -> PacketType {
+        match num {
+            0 => PacketType::Sum,
+            1 => PacketType::Product,
+            2 => PacketType::Min,
+            3 => PacketType::Max,
+            4 => PacketType::Literal,
+            5 => PacketType::Gt,
+            6 => PacketType::Lt,
+            7 => PacketType::Eq,
+            _ => panic!("Not a operation: {}", num)
         }
     }
 }
 
 #[derive(Debug)]
 struct PacketHeader {
-    version: usize,
+    version: isize,
     packet_type: PacketType
 }
 
@@ -69,7 +83,7 @@ impl PacketHeader {
 #[derive(Debug)]
 struct Packet {
     header: PacketHeader,
-    literal: Option<usize>,
+    literal: Option<isize>,
     children: Vec<Packet>
 }
 
@@ -82,7 +96,7 @@ impl Packet {
             PacketType::Literal => {
                 literal = Some(to_literal(iter));
             }
-            PacketType::Operation => {
+            _ => {
                 if *iter.next().unwrap() {
                     // 11 bits describing number of packets
                     let num_packets = to_num(&mut iter.take(11));
@@ -92,7 +106,7 @@ impl Packet {
                 } else {
                     // 15 bits describing size of packets
                     let packets_size = to_num(&mut iter.take(15));
-                    let mut next_iter = iter.take(packets_size).peekable();
+                    let mut next_iter = iter.take(packets_size as usize).peekable();
                     while next_iter.peek().is_some() {
                         children.push(Packet::from_iter(&mut next_iter));
                     }
@@ -106,13 +120,47 @@ impl Packet {
             children
         }
     }
+
+    fn execute(&self) -> isize {
+        match self.header.packet_type {
+            PacketType::Literal => {self.literal.unwrap()}
+            PacketType::Sum => {
+                self.children.iter().fold(0isize, |sum, child| {
+                    sum + child.execute()
+                })
+            }
+            PacketType::Product => {
+                self.children.iter().fold(1isize, |product, child| {
+                    product * child.execute()
+                })
+            }
+            PacketType::Min => {
+                self.children.iter().map(Packet::execute).min().unwrap()
+            }
+            PacketType::Max => {
+                self.children.iter().map(Packet::execute).max().unwrap()
+            }
+            PacketType::Gt => {
+                let values: Vec<isize> = self.children.iter().map(Packet::execute).collect();
+                (values[0] > values[1]) as isize
+            }
+            PacketType::Lt => {
+                let values: Vec<isize> = self.children.iter().map(Packet::execute).collect();
+                (values[0] < values[1]) as isize
+            }
+            PacketType::Eq => {
+                let values: Vec<isize> = self.children.iter().map(Packet::execute).collect();
+                (values[0] == values[1]) as isize
+            }
+        }
+    }
 }
 
-fn to_num(iter: &mut dyn Iterator<Item=BitRef<Const, Msb0, u8>>) -> usize {
-    iter.fold(0usize, |num, bit| (num << 1) | (*bit as usize))
+fn to_num(iter: &mut dyn Iterator<Item=BitRef<Const, Msb0, u8>>) -> isize {
+    iter.fold(0isize, |num, bit| (num << 1) | (*bit as isize))
 }
 
-fn to_literal(iter: &mut dyn Iterator<Item=BitRef<Const, Msb0, u8>>) -> usize {
+fn to_literal(iter: &mut dyn Iterator<Item=BitRef<Const, Msb0, u8>>) -> isize {
     let mut lit_bits = BitOwner::new();
     loop {
         let cont: bool = *iter.next().unwrap();
@@ -123,7 +171,7 @@ fn to_literal(iter: &mut dyn Iterator<Item=BitRef<Const, Msb0, u8>>) -> usize {
     to_num(&mut lit_bits.iter())
 }
 
-fn add_versions(packet: &Packet) -> usize {
+fn add_versions(packet: &Packet) -> isize {
     packet.children.iter()
         .fold(packet.header.version, |sum, packet| sum + add_versions(&packet))
 }
