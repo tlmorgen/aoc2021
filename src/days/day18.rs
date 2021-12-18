0072a0f46c66dfdad1a4c4278f953560255eaaa3
tlmorgen <tlmorgen@gmail.com>
2021-12-18 00:19:47 -0500

    init day 18

---
+++ src/days/day18.rs
@@ -0,0 +1,19 @@
+use super::super::day::Day;
+
+pub struct Day18 {}
+
+impl Day18 {
+    pub fn from_content(content: &str) -> Result<Box<dyn Day>, &'static str> {
+        Err("not implemented")
+    }
+}
+
+impl Day for Day18 {
+    fn part1(&mut self) -> isize {
+        0
+    }
+
+    fn part2(&mut self) -> isize {
+        0
+    }
+}
